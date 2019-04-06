13d7466cf3d979ad823020a611e63c2fa1aa8d24
hpbonfim <hp_bonfim@hotmail.com>
2019-04-05 21:39:23 -0400

    docker-compose

---
+++ react-sketch/firebase.js
@@ -0,0 +1,9 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://react-sketch-007.firebaseio.com"
+});
